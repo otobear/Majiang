e47c150db6a41977ce36e43cef0db551f43a285c
Yong Zuo <spacedogsergei@gmail.com>
2021-03-31 23:50:54 +0900

    增加统计数据界面

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     paipu_edit_single: './src/js/paipu_edit_single.js',
     paipu_edit_test: './src/js/paipu_edit_test.js',
     paipu_editer: './src/js/paipu_editer.js',
+    statistics: './src/js/statistics.js',
   },
   output: {
     path: __dirname + '/www/js/',
